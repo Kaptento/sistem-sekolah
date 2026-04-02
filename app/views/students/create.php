658b87907e6bcc1b9e45f50c6b8f8c58c2bdb26c
Kaptento <louisgracia456@gmail.com>
2026-04-02 13:34:02 +0700

    feat: impelement student index page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -1,12 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Siswa</title>
+    <link rel="stylesheet" href="/css/output.css">
 </head>
-<body>
-    <h1>Tambah Siswa</h1>
-    <p>Menampilkan form tambah siswa</p>
+
+<body class="min-h-screen flex flex-col bg-gray-100">
+    <!-- Header Start -->
+    <header class="bg-blue-500 text-white">
+        <div class="flex items-center justify-between container mx-auto p-4">
+            <a href="/students" class="font-bold text-xl">Sistem Sekolah</a>
+            <a href="/students/create" class="bg-white text-blue-500 px-4 py-2 rounded-lg">+ Tambah Siswa</a>
+        </div>
+    </header>
+    <!-- Header End -->
+
+    <!-- Main Start -->
+    <main class="container mx-auto grow">
+        <div class="mt-8 space-y-2">
+            <!-- Card Header Start -->
+            <div class="p-4 shadow rounded-lg bg-white">
+                <h1 class="text-2x1 font-bold">Tambah Siswa</h1>
+                <p>Menambahkan siswa ke dalam sistem</p>
+
+            </div>
+            <!-- Card Header End -->
+
+             <!-- card body Start -->
+                <div class="bg-white shadow rounded-lg bg-white">
+                    <table class="w-full">
+                     <thead class="bg-gray-200">
+                        <tr>
+                            <th class="px-4 py-2 text-left">No</th>
+                            <th class="px-4 py-2 text-left">Nama</th>
+                            <th class="px-4 py-2 text-left">NIS</th>
+                            <th class="px-4 py-2 text-left">Kelas</th>
+                            <th class="px-4 py-2 text-left">No Telepon</th>
+                            <th class="px-4 py-2">Aksi</th>
+                        </tr>
+                    <tbody>
+                        <tr>
+                            <td class="px-4 py-2 text-left">1</td>
+                            <td class="px-4 py-2 text-left">Andi</td>
+                            <td class="px-4 py-2 text-left">1234</td>
+                            <td class="px-4 py-2 text-left">11 TKJ 2</td>
+                            <td class="px-4 py-2 text-left">08123456789</td>
+                            <td class="px-4 py-2">
+                                <div class="flex justify-center items-center gap-4">
+                                <a href="" class="text-green-500">Detail</a>
+                                <a href="" class="text-yellow-500">Edit</a>
+                                <a href="" class="text-red-500">Hapus</a>
+                            </div>
+                        </td>
+                        </tr>
+                    </tbody>
+                    </thead>
+                    </table>
+                </div>
+             <!-- card body End -->
+        </div>
+    </main>
+    <!-- Main End -->
+
+    <!-- Footer Start -->
+    <footer class="bg-gray-800 text-white">
+        <div class="text-center p-4">
+            &copy <?= date(format: 'Y') ?> Sistem Sekolah - SMK Kristen Immanuel
+        </div>
+    </footer>
+    <!-- Footer End -->
+
+
 </body>
+
 </html>
