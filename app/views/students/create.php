b48bb5de9896f126d6c19a6f791e22b247cecee4
Kaptento <louisgracia456@gmail.com>
2026-04-02 14:01:19 +0700

    wip: implement student create page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -23,62 +23,48 @@
         <div class="mt-8 space-y-2">
             <!-- Card Header Start -->
             <div class="p-4 shadow rounded-lg bg-white">
-                <h1 class="text-2x1 font-bold">Tambah Siswa</h1>
+                <h1 class="text-2xl font-bold">Tambah Siswa</h1>
                 <p>Menambahkan siswa ke dalam sistem</p>
-
             </div>
             <!-- Card Header End -->
 
-            <!-- card body Start -->
-            <div class="bg-white shadow rounded-lg bg-white p-4">
+            <!-- Card Body Start -->
+            <div class="bg-white shadow rounded-lg p-4">
                 <form action="" class="grid grid-cols-2 gap-4">
                     <div class="space-y-2">
                         <label class="block font-bold" for="name">Nama</label>
                         <input class="w-full px-4 py-2 border rounded-lg" type="text" id="name"
-                            placeholder="Masukan nama" name="name">
+                            placeholder="Masukkan nama" name="name">
                     </div>
-
-                    <div class="bg-white shadow rounded-lg bg-white p-4">
-
-
-                        <div class="bg-white shadow rounded-lg bg-white p-4">
-                            <form action="">
-                                <div class="space-y-2">
-                                    <label class="block font-bold" for="nis">NIS</label>
-                                    <input class="w-full px-4 py-2 border rounded-lg" type="text" id="nis"
-                                        placeholder="Masukan NIS" name="nis">
-                                </div>
-                                <div class="space-y-2">
-                                    <label class="block font-bold" for="class">Kelas</label>
-                                    <input class="w-full px-4 py-2 border rounded-lg" type="text" id="class"
-                                        placeholder="Masukan nama" name="class">
-                                </div>
-
-                                <div class="bg-white shadow rounded-lg bg-white p-4">
-                                    <form action="">
-                                        <div class="space-y-2">
-                                            <label class="block font-bold" for="phone_number">No Telepon</label>
-                                            <input class="w-full px-4 py-2 border rounded-lg" type="text"
-                                                id="number_phone" placeholder="Masukan No Telepon" name="phone_number">
-                                        </div>
-                                    </form>
-                                </div>
-                                <!-- card body End -->
-                        </div>
-
+                    <div class="space-y-2">
+                        <label class="block font-bold" for="nis">NIS</label>
+                        <input class="w-full px-4 py-2 border rounded-lg" type="text" id="nis"
+                            placeholder="Masukkan NIS" name="nis">
                     </div>
+                    <div class="space-y-2">
+                        <label class="block font-bold" for="class">Kelas</label>
+                        <input class="w-full px-4 py-2 border rounded-lg" type="text" id="class"
+                            placeholder="Masukkan kelas" name="class">
+                    </div>
+                    <div class="space-y-2">
+                        <label class="block font-bold" for="phone_number">No Telepon</label>
+                        <input class="w-full px-4 py-2 border rounded-lg" type="text" id="phone_number"
+                            placeholder="Masukkan no telepon" name="phone_number">
+                    </div>
+                </form>
+            </div>
+            <!-- Card Body End -->
+        </div>
     </main>
     <!-- Main End -->
 
     <!-- Footer Start -->
     <footer class="bg-gray-800 text-white">
         <div class="text-center p-4">
-            &copy <?= date(format: 'Y') ?> Sistem Sekolah - SMK Kristen Immanuel
+            &copy <?= date('Y') ?> Sistem Sekolah - SMK Kristen Immanuel
         </div>
     </footer>
     <!-- Footer End -->
-
-
 </body>
 
 </html>
