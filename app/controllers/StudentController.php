31c6d7a6938e0606c50bf7dd691d1923e4fe55f4
Kaptento <louisgracia456@gmail.com>
2026-04-09 13:11:04 +0700

    feat: improvement view

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -1,30 +1,34 @@
 <?php
 namespace App\controllers;
+require_once '../app/core/controller.php';
 
-class StudentController
+use App\Core\Controller;
+
+class StudentController extends Controller
 {
 
 public function index()
 {
-        require_once '../app/views/students/index.php';
+
+        $this->view('students.index');
+
 }
 
 public function create()
 {
-        require_once '../app/views/students/create.php';
+           $this->view('students.create');
 }
 
 
 
 public function show(string $id)
 {
-        require_once '../app/views/students/show.php';
-
+        $this->view('students.show', ['id' => $id]);
 }
 
 public function edit(string $id)
 {
-        require_once '../app/views/students/edit.php';
+        $this->view('students.edit', ['id' => $id]);
 }
 
 
